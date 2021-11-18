b7ec301566dd2377832c63ec092fcd7c11350524
urregozw <urregozw@gmail.com>
2021-11-18 18:08:47 -0500

    Cambios generales a los metodos de interpolacion

--- ProyectoFinal/urrego.py
+++ ProyectoFinal/urrego.py
@@ -4,38 +4,40 @@ urrego = Blueprint('urrego', __name__,
                         template_folder='templates',
                         static_folder='static')
 
-@urrego.route('/methods/vandermonde')
-def vandermonde_route():
+@urrego.route('/methods/vandermonde/<string:array>', methods=['GET','POST'])
+def vandermonde_route(array):
     data = {}
     data['title'] = 'Vandermonde'
+    print(array)
     return render_template('interpolation/vandermonde.html', data = data)
 
-@urrego.route('/methods/newton')
-def newton_interpolation_route():
+@urrego.route('/methods/newton/<string:array>', methods=['GET','POST'])
+def newton_interpolation_route(array):
     data = {}
     data['title'] = 'Newton'
     return render_template('interpolation/newton.html', data = data)
 
-@urrego.route('/methods/lagrange')
-def lagrange_route():
+@urrego.route('/methods/lagrange/<int:array>', methods=['GET','POST'])
+def lagrange_route(array):
     data = {}
     data['title'] = 'Lagrange'
+    data['array_len'] = array
     return render_template('interpolation/lagrange.html', data = data)
 
-@urrego.route('/methods/spline_linear')
-def spline_linear_route():
+@urrego.route('/methods/spline_linear/<string:array>', methods=['GET','POST'])
+def spline_linear_route(array):
     data = {}
     data['title'] = 'Spline Linear'
     return render_template('interpolation/spline_linear.html', data = data)
 
-@urrego.route('/methods/spline_square')
-def spline_square_route():
+@urrego.route('/methods/spline_square/<string:array>', methods=['GET','POST'])
+def spline_square_route(array):
     data = {}
     data['title'] = 'Spline Square'
     return render_template('interpolation/spline_square.html', data = data)
 
-@urrego.route('/spline_cubica')
-def spline_cubic_route():
+@urrego.route('/methods/spline_cubica/<string:array>', methods=['GET','POST'])
+def spline_cubic_route(array):
     data = {}
     data['title'] = 'Spline Cubic'
     return render_template('interpolation/spline_cubic.html', data = data)
