acee721c5f5ed1ff9e58710c0a440f96bf8c8449
acastanop1 <53245202+acastanop1@users.noreply.github.com>
2021-11-18 21:56:07 -0500

    Added info for single variable methods

--- ProyectoFinal/alejandro.py
+++ ProyectoFinal/alejandro.py
@@ -1,4 +1,16 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request
+from io import StringIO
+import sys
+import math
+from sympy import sympify, evalf, symbols
+
+from Methods.Python.busquedaIncremental import busqueda
+from Methods.Python.biseccion import biseccion
+from Methods.Python.reglafalsa import reglafalsa
+from Methods.Python.puntofijo import puntofijo
+from Methods.Python.newton import newton
+from Methods.Python.secante import secante
+from Methods.Python.raicesmlt import raicesmlt
 
 alejandro = Blueprint('alejandro', __name__,
                         template_folder='templates',
@@ -6,4 +18,172 @@ alejandro = Blueprint('alejandro', __name__,
 
 @alejandro.route('/')
 def show():
-    return "HOLA Alejandro"
+    return render_template('singleVariable/incSearch.html')
+
+
+@alejandro.route('/methods/incsearch', methods=['GET', 'POST'])
+def incsearch_route():
+
+    if request.method == 'POST':
+        function = request.form['function']
+        xin = request.form['xin']
+        delta = request.form['delta']
+        nmax = request.form['nmax']
+
+        global function = sympify(function)
+
+        stdout = StringIO()
+        sys.stdout = stdout
+        x = busqueda(function, xin, delta, nmax)
+        result_stdout = stdout.getvalue()
+        result_stdout = result_stdout.split('\n')
+        return render_template('singleVariable/incSearch.html', x=x, stdout=result_stdout)
+
+    return render_template('singleVariable/incSearch.html')
+
+
+@alejandro.route('/methods/bisection', methods=['GET', 'POST'])
+def bisection_route():
+    def function(x):
+        return math.log(math.sin(x) ** 2 + 1) - 1 / 2
+    a = 0
+    b = 0.5
+    nmax = 100
+    tol = math.exp(-7)
+
+    if request.method == 'POST':
+        function = request.form['function']
+        a = request.form['a']
+        b = request.form['b']
+        nmax = request.form['nmax']
+        tol = request.form['tol']
+
+    stdout = StringIO()
+    sys.stdout = stdout
+    x = biseccion(function, a, b, nmax, tol)
+    result_stdout = stdout.getvalue()
+    result_stdout = result_stdout.split('\n')
+    return render_template('singleVariable/bisection.html', x=x, stdout = result_stdout)
+
+
+@alejandro.route('/methods/falsepos', methods=['GET', 'POST'])
+def falsepos_route():
+    def function(x):
+        return math.log(math.sin(x) ** 2 + 1) - 1 / 2
+    a = 0
+    b = 0.5
+    nmax = 100
+    tol = math.exp(-7)
+
+    if request.method == 'POST':
+        function = request.form['function']
+        a = request.form['a']
+        b = request.form['b']
+        nmax = request.form['nmax']
+        tol = request.form['tol']
+
+    stdout = StringIO()
+    sys.stdout = stdout
+    x = reglafalsa(function, a, b, nmax, tol)
+    result_stdout = stdout.getvalue()
+    result_stdout = result_stdout.split('\n')
+    return render_template('singleVariable/falsepos.html', x=x, stdout = result_stdout)
+
+
+@alejandro.route('/methods/fixedpoint', methods=['GET', 'POST'])
+def fixedpoint_route():
+    def function(x):
+        return math.log(math.sin(x) ** 2 + 1) - 1 / 2 - x
+
+    def functiong(x):
+        return math.log(math.sin(x) ** 2 + 1) - 1 / 2
+    x0 = 0
+    nmax = 100
+    tol = math.exp(-7)
+
+    if request.method == 'POST':
+        f = request.form['function']
+        g = request.form['fung']
+        xini = request.form['xini']
+        nmax = request.form['nmax']
+        tol = request.form['tol']
+
+    stdout = StringIO()
+    sys.stdout = stdout
+    x = puntofijo(function, g, xini, nmax, tol)
+    result_stdout = stdout.getvalue()
+    result_stdout = result_stdout.split('\n')
+    return render_template('singleVariable/fixedpoint.html', x=x, stdout = result_stdout)
+
+
+@alejandro.route('/methods/newtonraphs', methods=['GET', 'POST'])
+def newtonraphs_route():
+    def function(x):
+        return math.log(math.sin(x) ** 2 + 1) - 1 / 2 - x
+    x0 = 0
+    nmax = 100
+    tol = math.exp(-7)
+
+    if request.method == 'POST':
+        f = request.form['function']
+        fdx = request.form['fdx']
+        xini = request.form['xini']
+        nmax = request.form['nmax']
+        tol = request.form['tol']
+
+    stdout = StringIO()
+    sys.stdout = stdout
+    x = newton(f, fdx, xini, nmax, tol)
+    result_stdout = stdout.getvalue()
+    result_stdout = result_stdout.split('\n')
+    return render_template('singleVariable/newtonraphs.html', x=x, stdout = result_stdout)
+
+
+@alejandro.route('/methods/secant', methods=['GET', 'POST'])
+def secant_route():
+    def function(x):
+        return math.log(math.sin(x) ** 2 + 1) - 1 / 2 - x
+    x0 = 0
+    nmax = 100
+    tol = math.exp(-7)
+
+    if request.method == 'POST':
+        f = request.form['function']
+        x0 = request.form['xin']
+        x1 = request.form['x']
+        nmax = request.form['nmax']
+        tol = request.form['tol']
+
+    stdout = StringIO()
+    sys.stdout = stdout
+    x = secante(f, x0, x1, nmax, tol)
+    result_stdout = stdout.getvalue()
+    result_stdout = result_stdout.split('\n')
+    return render_template('singleVariable/newtonraphs.html', x=x, stdout = result_stdout)
+
+@alejandro.route('/methods/multiroots', methods=['GET', 'POST'])
+def multiroots_route():
+    def function(x):
+        return math.log(math.sin(x) ** 2 + 1) - 1 / 2 - x
+    x0 = 0
+    nmax = 100
+    tol = math.exp(-7)
+
+    if request.method == 'POST':
+        f = request.form['function']
+        fdx = request.form['fdx']
+        f2dx = request.form['f2dx']
+        x0 = request.form['xin']
+        nmax = request.form['nmax']
+        tol = request.form['tol']
+
+    stdout = StringIO()
+    sys.stdout = stdout
+    x = raicesmlt(f, fdx, f2dx,x0, nmax, tol)
+    result_stdout = stdout.getvalue()
+    result_stdout = result_stdout.split('\n')
+    return render_template('singleVariable/multipleroots.html', x=x, stdout = result_stdout)
+
+def funct(s):
+    x = symbols('x')
+    function.evalf(subs={x: s})
