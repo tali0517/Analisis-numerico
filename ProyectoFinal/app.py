52bcab4a27748e205b8996f8479d15b624f34d06
urregozw <urregozw@gmail.com>
2021-11-18 19:33:19 -0500

    Actualizacion methods interpolation y layout.py

--- ProyectoFinal/app.py
+++ ProyectoFinal/app.py
@@ -17,13 +17,20 @@ app.register_blueprint(tali, url_prefix='/tali')
 
 
 @app.route('/')
-def hello():
-    return render_template("index.html")
+def home():
+    return render_template("nav/index.html")
 
 @app.route('/methods')
 def methods_route():
-    return render_template("methods.html")
+    return render_template("nav/methods.html")
 
+@app.route('/aboutUs')
+def about_us_route():
+    return render_template("nav/about_us.html")
+
+@app.route('/help')
+def help_route():
+    return render_template("nav/help.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- ProyectoFinal/urrego.py
+++ ProyectoFinal/urrego.py
@@ -4,40 +4,104 @@ urrego = Blueprint('urrego', __name__,
                         template_folder='templates',
                         static_folder='static')
 
-@urrego.route('/methods/vandermonde/<string:array>', methods=['GET','POST'])
+@urrego.route('/methods/vandermonde/<int:array>', methods=['GET','POST'])
 def vandermonde_route(array):
     data = {}
     data['title'] = 'Vandermonde'
-    print(array)
+    
+    # Configuration of the input array
+    data['message'] = ''
+    if array < 1:
+        array = 1
+        data['message'] = "The array can't be less than zero"
+    elif array > 11:
+        array = 11
+        data['message'] = "The array can't be more than 11"    
+    data['array_len'] = array
+    
     return render_template('interpolation/vandermonde.html', data = data)
 
-@urrego.route('/methods/newton/<string:array>', methods=['GET','POST'])
+@urrego.route('/methods/newton/<int:array>', methods=['GET','POST'])
 def newton_interpolation_route(array):
     data = {}
     data['title'] = 'Newton'
+
+    # Configuration of the input array
+    data['message'] = ''
+    if array < 1:
+        array = 1
+        data['message'] = "The array can't be less than zero"
+    elif array > 11:
+        array = 11
+        data['message'] = "The array can't be more than 11"    
+    data['array_len'] = array
+
     return render_template('interpolation/newton.html', data = data)
 
 @urrego.route('/methods/lagrange/<int:array>', methods=['GET','POST'])
 def lagrange_route(array):
     data = {}
     data['title'] = 'Lagrange'
+    
+    # Configuration of the input array
+    data['message'] = ''
+    if array < 1:
+        array = 1
+        data['message'] = "The array can't be less than zero"
+    elif array > 11:
+        array = 11
+        data['message'] = "The array can't be more than 11"    
     data['array_len'] = array
+    
     return render_template('interpolation/lagrange.html', data = data)
 
-@urrego.route('/methods/spline_linear/<string:array>', methods=['GET','POST'])
+@urrego.route('/methods/spline_linear/<int:array>', methods=['GET','POST'])
 def spline_linear_route(array):
     data = {}
     data['title'] = 'Spline Linear'
+    
+    # Configuration of the input array
+    data['message'] = ''
+    if array < 1:
+        array = 1
+        data['message'] = "The array can't be less than zero"
+    elif array > 11:
+        array = 11
+        data['message'] = "The array can't be more than 11"    
+    data['array_len'] = array
+    
     return render_template('interpolation/spline_linear.html', data = data)
 
-@urrego.route('/methods/spline_square/<string:array>', methods=['GET','POST'])
+@urrego.route('/methods/spline_square/<int:array>', methods=['GET','POST'])
 def spline_square_route(array):
     data = {}
     data['title'] = 'Spline Square'
+    
+    # Configuration of the input array
+    data['message'] = ''
+    if array < 1:
+        array = 1
+        data['message'] = "The array can't be less than zero"
+    elif array > 11:
+        array = 11
+        data['message'] = "The array can't be more than 11"    
+    data['array_len'] = array
+    
     return render_template('interpolation/spline_square.html', data = data)
 
-@urrego.route('/methods/spline_cubica/<string:array>', methods=['GET','POST'])
+@urrego.route('/methods/spline_cubica/<int:array>', methods=['GET','POST'])
 def spline_cubic_route(array):
     data = {}
     data['title'] = 'Spline Cubic'
+    
+    # Configuration of the input array
+    data['message'] = ''
+    if array < 1:
+        array = 1
+        data['message'] = "The array can't be less than zero"
+    elif array > 11:
+        array = 11
+        data['message'] = "The array can't be more than 11"    
+    data['array_len'] = array
+    
     return render_template('interpolation/spline_cubic.html', data = data)
